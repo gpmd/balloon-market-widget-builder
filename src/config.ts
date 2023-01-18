81c2a7dc0f62e144317867f0e8e33864083c871b
James Basoo <jamesbasoo@gmail.com>
2023-01-18 16:12:20 +0000

    Change port

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const port: string = process.env.WIDGET_BUILDER_PORT || '8080';
+export const port: string = process.env.WIDGET_BUILDER_PORT || '3100';
 export const host: string = process.env.WIDGET_BUILDER_HOST || 'http://localhost';
